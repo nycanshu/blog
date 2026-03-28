e49fe913d364a347692ec914609a88c76d545db4
nycanshu <hkg43700@gmail.com>
2026-03-28 21:27:42 +0530

    Use GitHub Pages default URL without custom domain
    Switch to nycanshu.github.io/blog/ for testing before
    setting up custom domain.

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,8 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.nycanshu.dev",
+  site: "https://nycanshu.github.io",
+  base: "/blog",
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
